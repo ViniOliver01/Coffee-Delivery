049ecb0b1aefb14f538c5e1a69a3ebf783d5b185
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-01-20 11:47:33 -0300

    Updated header

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -1,36 +1,41 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin-block: 2rem;
+  height: 10vh;
+  gap: 2rem;
+`;
 
 export const LogoArea = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: center;
-    justify-content: flex-start;
+  display: flex;
+  flex: 1;
+  align-items: center;
+  justify-content: flex-start;
 
-    img{
-        width: 90px;
-        height: 40px;
-        cursor: pointer;
-    }
-`
+  img {
+    width: 90px;
+    height: 40px;
+    cursor: pointer;
+  }
+`;
 export const HeaderContainer = styled.div`
-    display: flex;
-    align-items: center;
-    margin-block: 2rem;
-    height: 10vh;
-    margin-inline: 10rem;
-    gap: 0.750rem;
-`
+  display: flex;
+  margin-inline: 10rem;
+  justify-content: space-between;
+`;
 export const LocationIcon = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 
-    border-radius: 8px;
-    padding: 0.5rem;
-    gap: 0.25rem;
-    background-color: ${(props) => props.theme['purple-light']};
-    color: ${(porps) => porps.theme['purple']};
+  border-radius: 8px;
+  padding: 0.5rem;
+  gap: 0.25rem;
+  background-color: ${(props) => props.theme["purple-light"]};
+  color: ${(porps) => porps.theme["purple"]};
 
-    p{
-        color: ${(props) => props.theme['purple-dark']}
-    }
-`
+  p {
+    color: ${(props) => props.theme["purple-dark"]};
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import logo from "../../assets/Logo.svg";
 import { CartContext } from "../../context/CartContext/CartContext";
 import Avatar from "../Avatar/Avatar";
 import { Cart } from "../Cart/Cart";
-import { HeaderContainer, LocationIcon, LogoArea } from "./Header.styles";
+import { Container, HeaderContainer, LocationIcon, LogoArea } from "./Header.styles";
 
 export function Header() {
   const { ItensAmount } = useContext(CartContext);
@@ -18,16 +18,20 @@ export function Header() {
 
   return (
     <HeaderContainer>
-      <LogoArea onClick={GoToHome}>
-        <img src={logo} />
-      </LogoArea>
+      <Container>
+        <LogoArea onClick={GoToHome}>
+          <img src={logo} />
+        </LogoArea>
 
-      <LocationIcon>
-        <MapPin size={24} weight="fill" />
-        <p>Porto Alegre, RS</p>
-      </LocationIcon>
-      <Avatar name="Vinicius" avatar_url="https://github.com/ViniOliver01.png" />
-      <Cart itens={ItensAmount} />
+        <LocationIcon>
+          <MapPin size={24} weight="fill" />
+          <p>Porto Alegre, RS</p>
+        </LocationIcon>
+      </Container>
+      <Container>
+        <Avatar name="Vinicius" avatar_url="https://github.com/ViniOliver01.png" />
+        <Cart itens={ItensAmount} />
+      </Container>
     </HeaderContainer>
   );
 }
