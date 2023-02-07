77a07288227694fe5c297acda6d46e754d4fcb08
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-02-07 20:24:42 -0300

    Updated Coffees and CartItem components

--- src/pages/Checkout/components/CartItemList/CartItem.tsx
+++ src/pages/Checkout/components/CartItemList/CartItem.tsx
@@ -1,78 +1,83 @@
-import { Minus, Plus, Trash } from 'phosphor-react';
-import { useContext } from 'react';
-import { CartContext } from '../../../../context/CartContext/CartContext';
-import { Buttons, CenterDiv, Container, ItensCountCart, PriceDisplay, RemoveItemButton } from './CartItem.styles';
-import { useState } from 'react';
-import { getImage } from './../../../../assets/CoffeeIndex';
+import { Minus, Plus, Trash } from "phosphor-react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../../../context/CartContext";
+import { formatCurrency } from "../../../../utils/format";
+import {
+  Buttons,
+  CenterDiv,
+  Container,
+  ItensCountCart,
+  PriceDisplay,
+  RemoveItemButton,
+} from "./CartItem.styles";
 
-interface CartItemProps{
-  id: number
-  name: string
-  img: string
-  price: number
-  amount: number
+interface CartItemProps {
+  id: string;
+  name: string;
+  img: string;
+  price: number;
+  amount: number;
 }
 
-export function CartItem({id, name, img, price, amount}:CartItemProps){
+export function CartItem({ id, name, img, price, amount }: CartItemProps) {
   const {
     handleAddItensToCart,
-    handleSetTotalAmount,
-    handleSetTotalPrice,
-    handleRemoveItensToCart
-  } = useContext(CartContext)
+    handleSetProductsAmount,
+    handleSetProductsPrice,
+    handleRemoveItensToCart,
+  } = useContext(CartContext);
   const [amountItens, setAmountItens] = useState(amount);
-  const totalPrice = price*amount
+  const totalPrice = (price / 100) * amount;
 
-  const FormatedPrice = new Intl.NumberFormat('pt-BR', {
-    style: 'decimal',
-    minimumFractionDigits : 2
-    }).format(totalPrice);
+  function handleAdd() {
+    setAmountItens(amountItens + 1);
+    const amount = amountItens + 1;
+    handleSetProductsAmount(+1);
+    handleSetProductsPrice(+price);
+    handleAddItensToCart({ id, name, img, price, amount });
+  }
 
-    function handleAdd(){
-      setAmountItens(amountItens+1)
-      const amount = amountItens+1
-      handleSetTotalAmount(+1)
-      handleSetTotalPrice(+price)
-      handleAddItensToCart({id, name, img, price, amount})
+  function handleMinus() {
+    setAmountItens(amountItens - 1);
+    const amount = amountItens - 1;
+    if (amount == 0) {
+      handleRemoveItensToCart({ id, name, img, price, amount });
+    } else {
+      handleAddItensToCart({ id, name, img, price, amount });
     }
+    handleSetProductsAmount(-1);
+    handleSetProductsPrice(-price);
+  }
 
-    function handleMinus(){
-      setAmountItens(amountItens-1)
-      const amount = amountItens-1
-      if (amount ==0) {
-        handleRemoveItensToCart({id, name, img, price, amount})
-      }else{
-        handleAddItensToCart({id, name, img, price, amount})
-      }
-      handleSetTotalAmount(-1)
-      handleSetTotalPrice(-price)
-    }
-
-    function handleRemove(){
-      handleSetTotalAmount(-amount)
-      handleSetTotalPrice(-price*amount)
-      handleRemoveItensToCart({id, name, img, price, amount})
-    }
+  function handleRemove() {
+    handleSetProductsAmount(-amount);
+    handleSetProductsPrice(-price * amount);
+    handleRemoveItensToCart({ id, name, img, price, amount });
+  }
 
   return (
     <Container>
-        <img src={getImage(id)} alt="" />
-        <CenterDiv>
-            <p>{name}</p>
-            <Buttons>
-                <ItensCountCart>
-                <a onClick={handleMinus}><Minus size={16} weight="bold"/></a>
-                {amountItens}
-                <a onClick={handleAdd}><Plus size={16} weight="bold" /></a>
-                </ItensCountCart>
+      <img src={img} alt="" />
+      <CenterDiv>
+        <p>{name}</p>
+        <Buttons>
+          <ItensCountCart>
+            <a onClick={handleMinus}>
+              <Minus size={16} weight="bold" />
+            </a>
+            {amountItens}
+            <a onClick={handleAdd}>
+              <Plus size={16} weight="bold" />
+            </a>
+          </ItensCountCart>
 
-                <RemoveItemButton onClick={handleRemove}>
-                    <Trash size={16}/>
-                    <p>REMOVER</p>
-                </RemoveItemButton>
-            </Buttons>
-        </CenterDiv>
-        <PriceDisplay>R$ {FormatedPrice}</PriceDisplay>
+          <RemoveItemButton onClick={handleRemove}>
+            <Trash size={16} />
+            <p>REMOVER</p>
+          </RemoveItemButton>
+        </Buttons>
+      </CenterDiv>
+      <PriceDisplay>R$ {formatCurrency(totalPrice)}</PriceDisplay>
     </Container>
   );
 }
--- src/pages/Home/components/coffees/Coffees.tsx
+++ src/pages/Home/components/coffees/Coffees.tsx
@@ -1,100 +1,118 @@
-import { CartButton, Container, ItensCount, ShopSection, Price, IconCartButton } from "./Coffees.styles";
-import { Plus, Minus, ShoppingCartSimple} from 'phosphor-react'
-import { useContext, useEffect, useState } from "react";
-import { CartContext } from "../../../../context/CartContext/CartContext";
-import { getImage } from "../../../../assets/CoffeeIndex";
+import { Minus, Plus, ShoppingCartSimple } from "phosphor-react";
+import { useContext, useState } from "react";
+import { CartContext } from "../../../../context/CartContext";
+import { formatCurrency } from "../../../../utils/format";
+import {
+  CartButton,
+  Container,
+  IconCartButton,
+  ItensCount,
+  Price,
+  ShopSection,
+} from "./Coffees.styles";
 
-interface CoffeesProps{
-    id: number
-    type: string[]
-    name: string
-    img: string
-    description: string
-    price: number
-    children?: React.ReactNode
+interface CoffeesProps {
+  id: string;
+  specifications: {
+    id: string;
+    name: string;
+    created_at: Date;
+  }[];
+  name: string;
+  img: string;
+  description: string;
+  price: number;
+  children?: React.ReactNode;
 }
 
-export function Coffees({id, type, name, img, description, price}: CoffeesProps){
+export function Coffees({
+  id,
+  specifications,
+  name,
+  img,
+  description,
+  price,
+}: CoffeesProps) {
   const [itensCount, setItensCount] = useState(1);
-  const [efect, setEfect] = useState(false)
-  const [textCartAddButton, setTextCartAddButton] = useState('Adicionar ao carrinho')
+  const [effect, setEffect] = useState(false);
+  const [textCartAddButton, setTextCartAddButton] = useState("Adicionar ao carrinho");
 
-  const triggerEfect = () => {
-    setEfect(true)
-    setTextCartAddButton('Adicionado com sucesso')
-    setTimeout(() => setEfect(false), 2000);
-    setTimeout(() => setTextCartAddButton('Adicionar ao carrinho'), 2000);
-    
-  }
+  const triggerEffect = () => {
+    setEffect(true);
+    setTextCartAddButton("Adicionado com sucesso");
+    setTimeout(() => setEffect(false), 2000);
+    setTimeout(() => setTextCartAddButton("Adicionar ao carrinho"), 2000);
+  };
 
   const {
-    handleSetTotalAmount,
+    handleSetProductsAmount,
     handleAddItensToCart,
-    handleSetTotalPrice,
-    handleCheckObjectsState
-    } = useContext(CartContext)
-  
-  function MinusItem(){
-    if (itensCount!=1) {
-      setItensCount(state => state-1);
+    handleSetProductsPrice,
+    handleCheckObjectsState,
+  } = useContext(CartContext);
+
+  function MinusItem() {
+    if (itensCount != 1) {
+      setItensCount((state) => state - 1);
     }
   }
 
-  function PlusItem(){
-    setItensCount(state => state+1);
+  function PlusItem() {
+    setItensCount((state) => state + 1);
   }
 
-  function AddItemToCart(e: any){
-    triggerEfect()
+  function AddItemToCart(e: any) {
+    triggerEffect();
 
     let amount = itensCount;
-    handleSetTotalPrice(price*amount)
-    const newAmount = handleCheckObjectsState({id, name, img, price, amount})
-    handleSetTotalAmount(amount)
-    amount = newAmount+amount
-    handleAddItensToCart({id, name, img, price, amount})
+    handleSetProductsPrice(price * amount);
+    const newAmount = handleCheckObjectsState({ id, name, img, price, amount });
+    handleSetProductsAmount(amount);
+    amount = newAmount + amount;
+    handleAddItensToCart({ id, name, img, price, amount });
     setItensCount(1);
   }
 
-  const FormatedPriceToBrazilianReal = new Intl.NumberFormat('pt-BR', {
-    style: 'decimal',
-    minimumFractionDigits : 2
-    }).format(price);
-    
   return (
     <Container>
-        <img src={getImage(id)} alt="" />
-        <ul>
-          {type.map(item => {
-            return(
-              <li key={item}>{item}</li>
-            )
-          })}
-        </ul>
-        <h2>{name}</h2>
-        <h3>{description}</h3>
-        <ShopSection>
-          <Price><p>R$<span>{FormatedPriceToBrazilianReal}</span></p></Price>
-          
-          <ItensCount>
-            <a onClick={MinusItem}><Minus size={16} weight="bold"/></a>
-            {itensCount}
-            <a onClick={PlusItem}><Plus size={16} weight="bold" /></a>
-          </ItensCount>
+      <img src={img} alt="" />
+      <ul>
+        {specifications.map((item) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+      <h2>{name}</h2>
+      <h3>{description}</h3>
+      <ShopSection>
+        <Price>
+          <p>
+            R$<span>{formatCurrency(price / 100)}</span>
+          </p>
+        </Price>
 
-          <CartButton 
-            onClick={AddItemToCart} 
-            onAnimationEnd={triggerEfect} 
-            className={efect ? 'addCartEfect' : ''}>
-            <p>{textCartAddButton}</p>
+        <ItensCount>
+          <a onClick={MinusItem}>
+            <Minus size={16} weight="bold" />
+          </a>
+          {itensCount}
+          <a onClick={PlusItem}>
+            <Plus size={16} weight="bold" />
+          </a>
+        </ItensCount>
 
-            <IconCartButton>
-              <span> {efect ? <Plus size={10}/> : ''} </span>
-              <ShoppingCartSimple size={16} weight='fill'></ShoppingCartSimple>
-            </IconCartButton>
-  
-          </CartButton>
-        </ShopSection>
+        <CartButton
+          onClick={AddItemToCart}
+          onAnimationEnd={triggerEffect}
+          className={effect ? "addCartEfect" : ""}
+        >
+          <p>{textCartAddButton}</p>
+
+          <IconCartButton>
+            <span> {effect ? <Plus size={10} /> : ""} </span>
+            <ShoppingCartSimple size={16} weight="fill"></ShoppingCartSimple>
+          </IconCartButton>
+        </CartButton>
+      </ShopSection>
     </Container>
   );
 }
