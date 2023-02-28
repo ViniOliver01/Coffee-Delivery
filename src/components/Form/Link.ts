932261f9d58e6c5cba281eb6cf6832ccd6494a67
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-02-28 15:27:18 -0300

    updated Link

--- src/components/Form/Link.ts
+++ src/components/Form/Link.ts
@@ -1,11 +1,29 @@
 import styled from "styled-components";
 import defaultTheme from "../../styles/themes/Default";
 
+interface LinkProps {
+  hasIcon?: boolean;
+}
+
 export const Link = styled.a`
   color: ${defaultTheme.purple};
   cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  width: fit-content;
+
+  border-bottom: 1px solid transparent;
+
+  svg {
+    font-size: 1rem;
+  }
 
   &:hover {
     background-color: ${defaultTheme["purple-light"]};
+
+    border-bottom: 1px solid
+      ${(props: LinkProps) =>
+        props.hasIcon ? defaultTheme["purple-dark"] : "transparent"};
   }
 `;
