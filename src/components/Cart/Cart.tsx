1042ca9479df87b94bc35ecd2073f35177cb2dea
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2023-03-13 17:20:27 -0300

    updated

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@chakra-ui/react";
 import { ShoppingCart } from "phosphor-react";
 import { useNavigate } from "react-router-dom";
 import { formatCurrency } from "../../utils/format";
@@ -15,16 +16,23 @@ export function Cart({ totalItens, totalPrice }: CartProps) {
     navigate("/checkout");
   }
 
+  const [isMobile] = useMediaQuery("(max-width: 700px)", {
+    ssr: true,
+    fallback: false,
+  });
+
   return (
     <Container>
       <CartBox onClick={GoToCart}>
         <ShoppingCart size={22} weight="fill" />
         <CountItens>{totalItens}</CountItens>
       </CartBox>
-      <div>
-        <p>Total</p>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
+      {!isMobile && (
+        <div>
+          <p>Total</p>
+          <p>{formatCurrency(totalPrice)}</p>
+        </div>
+      )}
     </Container>
   );
 }
